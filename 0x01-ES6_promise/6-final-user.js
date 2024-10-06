bdc974258fc0fa501f35812c90ed14ae571ce16f
enejepromise <enejepromise@gmail.com>
2024-10-06 13:50:22 +0000

    mandatory task

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,12 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((res) => res.map((res) => ({
+    status: res.status,
+    value: res.status === 'fulfilled' ? res.value : String(res.reason),
+  })));
+}
