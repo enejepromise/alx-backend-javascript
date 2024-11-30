0a7d1c99a775cb239d11b1ec382c848667464c34
enejepromise <enejepromise@gmail.com>
2024-11-30 04:15:59 +0000

    unittest

---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,23 @@
+class Utils {
+  static calculateNumber(type, a, b) {
+    const c = Math.round(a);
+    const d = Math.round(b);
+
+    if (type === 'SUM') {
+      return c + d;
+    }
+
+    if (type === 'SUBTRACT') {
+      return c - d;
+    }
+
+    if (type === 'DIVIDE') {
+      if (d === 0) {
+        return 'Error';
+      }
+      return c / d;
+    }
+    return 'Invalid operation type';
+  }
+}
+
